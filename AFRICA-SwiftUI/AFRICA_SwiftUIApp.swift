0fc713ad775e0a60e56a5b377f1d1ee5148ecbda
monir <mdmonirhelale@gmail.com>
2024-05-29 23:18:43 +0600

    Initial Commit

---
+++ AFRICA-SwiftUI/AFRICA_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFRICA_SwiftUIApp.swift
+//  AFRICA-SwiftUI
+//
+//  Created by Monir Haider Helalee on 29/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AFRICA_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFRICA-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AFRICA-SwiftUI
+//
+//  Created by Monir Haider Helalee on 29/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
