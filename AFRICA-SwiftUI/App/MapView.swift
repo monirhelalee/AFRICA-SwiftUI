d55ab3e0639be6561229b80e6047a51c3b4e6ffa
monir <mdmonirhelale@gmail.com>
2024-06-06 11:44:29 +0600

    implement custom annotation in map

--- AFRICA-SwiftUI/App/MapView.swift
+++ AFRICA-SwiftUI/App/MapView.swift
@@ -5,43 +5,71 @@
 //  Created by Monir Haider Helalee on 29/5/24.
 //
 
-import SwiftUI
 import MapKit
+import SwiftUI
 
 struct MapView: View {
     // MARK: - PROPERTIES
+
     @State private var region: MKCoordinateRegion = {
         var mapCoordinates = CLLocationCoordinate2D(latitude: 6.60028, longitude: 16.4377599)
         var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
         var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
-        
+
         return mapRegion
     }()
-    
+
     let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+
     // MARK: - BODY
 
     var body: some View {
         // MARK: - No1 Basic Map
-        //Map(coordinateRegion: $region)
-        
+
+        // Map(coordinateRegion: $region)
+
         // MARK: - No2 Advanced Map
+
         Map(coordinateRegion: $region, annotationItems: locations,
             annotationContent: { item in
-            // (A) Pin: old style (Static)
-            //MapPin(coordinate: item.location, tint: .accentColor)
-            
-            // (B) Marker: New Style (Static)
-            //MapMarker(coordinate: item.location, tint: .accentColor)
-            
-            // (C) Custom basic annotation (interactive)
-            MapAnnotation(coordinate: item.location){
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 32, height: 32, alignment: .center)
-            } //: Annotation
-        })
+                // (A) Pin: old style (Static)
+                // MapPin(coordinate: item.location, tint: .accentColor)
+
+                // (B) Marker: New Style (Static)
+                // MapMarker(coordinate: item.location, tint: .accentColor)
+
+                // (C) Custom basic annotation (interactive)
+                // MapAnnotation(coordinate: item.location){
+                // Image("logo")
+                // .resizable()
+                // .scaledToFit()
+                // .frame(width: 32, height: 32, alignment: .center)
+                // } //: Annotation
+                // (D) Custom Advanced annotation (interactive)
+                MapAnnotation(coordinate: item.location) {
+                    MapAnnotationView(location: item)
+                }
+            }) //: MAP
+            .overlay(
+                HStack(alignment: .center, spacing: 12) {
+                    Image("compass")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 48, height: 48, alignment: .center)
+                    VStack(alignment: .leading, spacing: 3){
+                        Divider()
+                    }
+                } //: Hstack
+                .padding(.vertical, 12)
+                .padding(.horizontal, 12)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding(),
+                alignment: .top
+            )
     }
 }
 
