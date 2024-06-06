f79a5e821b032d3fac1288403f134486b385b1d3
monir <mdmonirhelale@gmail.com>
2024-06-07 01:29:51 +0600

    add two button in toolbar of contain view

--- AFRICA-SwiftUI/App/ContentView.swift
+++ AFRICA-SwiftUI/App/ContentView.swift
@@ -9,25 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTIES
+
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    @State private var isGridViewActive: Bool = false
+
     // MARK: - BODY
 
     var body: some View {
-        NavigationView{
-            List{
+        NavigationView {
+            List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals){ animal in
-                    NavigationLink(destination:AnimalDetailView(animal: animal)){
+
+                ForEach(animals) { animal in
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
                         AnimalListItemView(animal: animal)
-                    }//: Navigationlink
-                    
-                }//: Loop
-            }//: List
+                    } //: Navigationlink
+                } //: Loop
+            } //: List
             .navigationBarTitle("Africa", displayMode: .large)
-        }//: Navigation
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // List
+                        Button(action: {
+                            print("list")
+                            isGridViewActive = false
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+
+                        // Grid
+                        Button(action: {
+                            print("Grid")
+                            isGridViewActive = true
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    } //: HStack
+                } //: Button
+            } //: Toolbar
+        } //: Navigation
     }
 }
 
